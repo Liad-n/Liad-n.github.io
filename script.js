e083137dc7abfeeffd7efe35c91623497b6f8794
Liad Noam <LiadN@Taldor.corp>
2020-03-27 12:24:29 +0300

    test script in js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAuh6kndYOoghVBnDeFvbSGgwSKw4HJutU",
+    authDomain: "liad-n.firebaseapp.com",
+    databaseURL: "https://liad-n.firebaseio.com",
+    projectId: "liad-n",
+    storageBucket: "liad-n.appspot.com",
+    messagingSenderId: "741742282809",
+    appId: "1:741742282809:web:d4fce9ac8b5b83753065e8",
+    measurementId: "G-1PM5Z6T8NZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log('hi')
+  firebase.analytics();
+
 function saveToFirebase(email) {
     var emailObject = {
         email: email
